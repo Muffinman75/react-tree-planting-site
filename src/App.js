41f4f048529c8ded9feecd4a8b133f6a5f26c04c
Manoj Modhwadia <manojmodhwadia@outlook.com>
2020-06-22 16:55:32 +0100

    very basic architecture and have data in app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [treeData, setTreeData] = useState(false);
+
+  async function getTrees() {
+    await axios.get("https://api.offset.earth/trees")
+    .then(result => {
+      if (result.status === 200) {
+        console.log("result", result);
+        setTreeData(result.data);
+      } else {
+        console.log('error');
+      }
+    }).catch(e => {
+      console.log('catch error');
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button type="button" className="submit" onClick={getTrees}>
+          Get Trees
+        </button>
+        {treeData &&
+          treeData.map((tree, index) => (
+              <div key={index}>
+                <h2>
+                  {tree.value}
+                </h2>
+                <p>
+                  {tree.createdAt}
+                </p>
+              </div>
+            ))
+        }
       </header>
     </div>
   );
 }
-
 export default App;
